413cc6e94d8b1785101c10ece4edba7654d624a7
Arveen <arvkmr@users.noreply.github.com>
2020-11-23 17:02:29 +0000

    Add basic UI

--- AppAdaptriOS/AppDelegate.m
+++ AppAdaptriOS/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Adaptr/Adaptr.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [AdaptrAudioPlayer setClientToken:@"demo" secret:@"demo"];
     return YES;
 }
 
--- AppAdaptriOS/ViewController.m
+++ AppAdaptriOS/ViewController.m
@@ -6,9 +6,16 @@
 //
 
 #import "ViewController.h"
+#import <Adaptr/Adaptr.h>
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet MetadataLabel *track;
+@property (weak, nonatomic) IBOutlet MetadataLabel *artist;
+@property (weak, nonatomic) IBOutlet MetadataLabel *album;
+
+@property NSUInteger noOfstations;
 @end
 
 @implementation ViewController
@@ -16,7 +23,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    AdaptrAudioPlayer *player = [AdaptrAudioPlayer sharedPlayer];
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    [player whenAvailable:^{
+        //player.doesHandleRemoteCommands = NO;
+        [player setSecondsOfCrossfade:0];
+        for (Station *station in player.stationList) {
+            self->_noOfstations++;
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+            [button addTarget:self action:@selector(changeStation:) forControlEvents:UIControlEventTouchUpInside];
+            [[button layer] setValue:station forKey:@"station"];
+            [button setTitle:station.name forState:UIControlStateNormal];
+            
+            button.frame = CGRectMake((width/4), 50.0* (double)self->_noOfstations, 150.0, 50.0);
+            [self.view addSubview:button];
+            
+            //NSLog( @"Size =%lu", [player getOfflineStorageUsed]);
+        }
+    } notAvailable:^{
+        NSLog(@"Not available");
+    }];
 }
+    
+ - (void)changeStation: (UIButton*)sender
+ {
+     Station *station = [[sender layer] valueForKey:@"station"];
+     [[AdaptrAudioPlayer sharedPlayer] setActiveStation:station withCrossfade:NO];
+     [[AdaptrAudioPlayer sharedPlayer] play];
+ }
+
+  
 
 
 @end
